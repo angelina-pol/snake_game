91e28d97a89b974e8c593a3f33676999089980ec
Angelina <gela-turlova@inbox.ru>
2024-02-27 00:03:57 +0300

    added arrow navigation

--- src/components/Navigation/Arrow.tsx
+++ src/components/Navigation/Arrow.tsx
@@ -1,13 +1,18 @@
 import './Arrow.css';
+import store from '../../store/MainStore';
 
 type ArrowProps = {
   typeOfArrow: string;
+  newType: string;
+  typeBefore: string;
+  setTypeBefore: (newType: string) => void;
 }
 
-const Arrow: React.FC<ArrowProps> = ({ typeOfArrow  }) => {
+const Arrow: React.FC<ArrowProps> = ({ typeOfArrow, newType, typeBefore, setTypeBefore }) => {
+  setTypeBefore(newType);
   return (
     <div className="arrow">
-      {typeOfArrow}
+      <button className="button" onClick={() => store.changeDirection(newType, typeBefore)}>{typeOfArrow}</button>
     </div>
   );
 };
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,26 +1,29 @@
-import './Arrow.css';
+import './Navigation.css';
 import Arrow from './Arrow';
+import { useState } from 'react';
 
 type NavigationProps = {
+
 }
 
 const Navigation: React.FC<NavigationProps> = () => {
+  const [typeBefore, setTypeBefore] = useState<string>('right');
   return (
     <div className="navigation">
       <table>
         <tr>
           <td></td>
-          <td><Arrow  typeOfArrow="&#9650;"/></td>
+          <td><Arrow  typeOfArrow="&#9650;" newType="up" typeBefore={typeBefore} setTypeBefore={setTypeBefore}/></td>
           <td></td>
         </tr>
         <tr>
-          <td><Arrow  typeOfArrow="&#9664;"/></td>
+          <td><Arrow  typeOfArrow="&#9664;" newType="left" typeBefore={typeBefore} setTypeBefore={setTypeBefore}/></td>
           <td></td>
-          <td><Arrow  typeOfArrow="&#9654;"/></td>
+          <td><Arrow  typeOfArrow="&#9654;" newType="right" typeBefore={typeBefore} setTypeBefore={setTypeBefore}/></td>
         </tr>
         <tr>
           <td></td>
-          <td><Arrow  typeOfArrow="&#9660;"/></td>
+          <td><Arrow  typeOfArrow="&#9660;" newType="down" typeBefore={typeBefore} setTypeBefore={setTypeBefore}/></td>
           <td></td>
         </tr>
       </table>
